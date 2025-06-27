f328123fd5f3bebb4d5388763559f939b6f09a8a
Lucia Vu <luciavu6@outlook.com>
2025-06-28 07:15:12 +1000

    style: add horizontal divider between projects

--- src/components/MainProject/MainProject.tsx
+++ src/components/MainProject/MainProject.tsx
@@ -22,36 +22,39 @@ const MainProject = ({
   techstack,
 }: MainProjectPropTypes) => {
   return (
-    <div className="main-project" id={`project${id}`}>
-      <ImageCarousel images={imagePreviews}></ImageCarousel>
-      <div className="project-details flex-container">
-        <div>
-          <h1>
-            <a href={liveLink} target="_blank">
-              {title}
-            </a>
-          </h1>
-          <div className="links">
-            <a href={liveLink} target="_blank" className="link">
-              Live link
-            </a>
-            <a href={codeLink} target="_blank" className="link">
-              View code
-            </a>
+    <>
+      <div className="main-project" id={`project${id}`}>
+        <ImageCarousel images={imagePreviews}></ImageCarousel>
+        <div className="project-details flex-container">
+          <div>
+            <h1>
+              <a href={liveLink} target="_blank">
+                {title}
+              </a>
+            </h1>
+            <div className="links">
+              <a href={liveLink} target="_blank" className="link">
+                Live link
+              </a>
+              <a href={codeLink} target="_blank" className="link">
+                View code
+              </a>
+            </div>
           </div>
-        </div>
 
-        <div className="techstack">
-          {techstack.map((Icon, idx) => (
-            <Icon key={idx} />
-          ))}
-        </div>
+          <div className="techstack">
+            {techstack.map((Icon, idx) => (
+              <Icon key={idx} />
+            ))}
+          </div>
 
-        <p>{description1}</p>
-        <p>{description2}</p>
+          <p>{description1}</p>
+          <p>{description2}</p>
+        </div>
       </div>
+
       <hr />
-    </div>
+    </>
   );
 };
 
